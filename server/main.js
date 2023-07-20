25c81b72acf4cdf1b8e8bf7edcb1d344f07fd414
devlsy86 <infightersy@naver.com>
2023-07-21 05:36:13 +0900

    https://velopert.com/1921 3. 미들웨어 및 기타 모듈 설치 할 차례

--- server/main.js
+++ server/main.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import path from 'path';
 
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+
 const app = express();
 const port = 3000;
+
+const devPort = 4000;
+
+/*
+    Express Codes 
+*/
 
 app.use('/', express.static(path.join(__dirname, './../public')));
 
@@ -13,3 +22,15 @@ app.get('/hello', (req, res) => {
 app.listen(port, () => {
     console.log('Express is listening on port', port);
 });
+
+if(process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port', devPort);
+        }
+    );
+}
