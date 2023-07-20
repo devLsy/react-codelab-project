71cf5a5572671b15ca55cdcf529a1ae43798c945
devlsy86 <infightersy@naver.com>
2023-07-21 06:02:55 +0900

    import api from './routes'; 여기서 에러 나는 중

--- server/main.js
+++ server/main.js
@@ -12,20 +12,19 @@ import session from 'express-session';
 
 /* setup routers & static directory */
 import api from './routes';
+app.use('/api', api);
 
-const app = express();
+const app = express();  
 const port = 3000;
 
 const devPort = 4000;
-
+    
 /* mongodb connection */
 const db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', () => { console.log('Connected to mongodb server'); });
 // mongoose.connect('mongodb://username:password@host:port/database=');
 mongoose.connect('mongodb://localhost/codelab');
-
-app.use('/api', api);
 
 /* use session */
 app.use(session({
